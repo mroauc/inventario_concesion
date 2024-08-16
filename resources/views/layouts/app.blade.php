ab5d7d6013168613ded70231b24fa3ce6700ed47
mRoa19 <mroa@hbl.cl>
2024-08-15 23:36:01 -0400

    titulo pagina

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>{{ config('app.name') }}</title>
+    <title>Inventario</title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     
     <!-- Font Awesome -->
