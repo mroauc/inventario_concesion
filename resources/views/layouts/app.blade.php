0dbd15433eb0c136985d557a08738ff0b12da6d2
mRoa19 <mroa@hbl.cl>
2024-11-30 14:52:37 -0300

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Inventario</title>
+    <meta property="og:title" content="Inventario">
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     
     <!-- Font Awesome -->
