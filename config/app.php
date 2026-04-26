20852f32e410928b157058989cebf93f38549e8b
mroa19mc <mroa@hbl.cl>
2026-04-26 18:36:28 -0400

    Remove InfyOm provider from production config

--- config/app.php
+++ config/app.php
@@ -174,7 +174,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        InfyOm\Generator\InfyOmGeneratorServiceProvider::class,
         Maatwebsite\Excel\ExcelServiceProvider::class,
 
     ],
